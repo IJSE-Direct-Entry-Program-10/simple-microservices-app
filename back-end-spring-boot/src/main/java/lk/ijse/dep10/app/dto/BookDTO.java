231f52c10340010def10010798b1ab34eb99b29f
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-06-28 20:56:38 +0530

    Finalize API Layer

--- back-end-spring-boot/src/main/java/lk/ijse/dep10/app/dto/BookDTO.java
+++ back-end-spring-boot/src/main/java/lk/ijse/dep10/app/dto/BookDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Data
@@ -12,6 +13,8 @@ import java.io.Serializable;
 @AllArgsConstructor
 @Builder
 public class BookDTO implements Serializable {
+    @NotBlank(message = "ISBN can't be empty")
     private String isbn;
+    @NotBlank(message = "Title can't be empty")
     private String title;
 }
