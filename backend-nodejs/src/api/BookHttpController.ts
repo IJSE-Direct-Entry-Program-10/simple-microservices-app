75e42302339b79c479e51267774f2a31e11cf18c
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-06-28 19:26:25 +0530

    Expose following web services
    DELETE http://localhost:8080/api/v1/books/{isbn}
    UPDATE http://localhost:8080/api/v1/books/{isbn}

--- backend-nodejs/src/api/BookHttpController.ts
+++ backend-nodejs/src/api/BookHttpController.ts
@@ -1,3 +1,11 @@
 import express from 'express';
 
 export const router = express.Router();
+
+router.delete('/:isbn', (req, res) => {
+    res.send("<h1>Delete a book</h1>");
+});
+
+router.patch('/:isbn', (req, res) => {
+    res.send("<h1>Update a book</h1>");
+});
